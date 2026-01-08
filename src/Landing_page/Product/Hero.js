c375bce511fc2ee953e44828ee4dd35e61397d45
Piyush <crazyengineer006@gmail.com>
2026-01-08 23:51:25 +0530

    Product section completed

--- src/Landing_page/Product/Hero.js
+++ src/Landing_page/Product/Hero.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className='container p-5 border-bottom'>
+            <div className='row p-5'>
+                <h2 style={{color:"#424242",textAlign:"center",fontSize:"30px",paddingBottom:"8px"}}>Zerodha Products</h2>
+                <p style={{color:"#424242",textAlign:"center",fontSize:"20px",paddingBottom:"8px"}}>Sleek, modern, and intuitive trading platforms</p>
+                <p style={{color:"#424242",textAlign:"center",fontSize:"17px"}}>Check out our <a href=" " style={{textDecoration:"none"}}>investment offerings →</a></p>
+            </div>
+            
+        </div>
      );
 }
 
